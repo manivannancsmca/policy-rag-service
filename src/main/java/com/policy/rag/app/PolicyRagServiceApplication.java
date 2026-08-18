0abac6773775b75ae901bc604db2c3baa215a41b
manivannancsmca <manivannancsmca@gmail.com>
2026-08-18 14:55:07 +0530

    first commit

---
+++ src/main/java/com/policy/rag/app/PolicyRagServiceApplication.java
@@ -0,0 +1,13 @@
+package com.policy.rag.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyRagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyRagServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/policy/rag/app/PolicyRagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policy.rag.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyRagServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
